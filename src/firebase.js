3ed488d752b8db274d88b114229b7aca8a0d3133
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-03-31 19:10:41 +0200

    firebase configuration for simple deploy

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAr47HkUx9SJAHWKP-Zl0O0SVXbFzHFuPc",
+    authDomain: "todo-14d41.firebaseapp.com",
+    projectId: "todo-14d41",
+    storageBucket: "todo-14d41.appspot.com",
+    messagingSenderId: "295073190890",
+    appId: "1:295073190890:web:43403460abc423c2819893"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
